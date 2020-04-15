62944eafdd9f770c2196892b12b3a7c5fc96b1c5
Lionel Tolosa <lioneltolosaarmas@gmail.com>
2020-04-15 17:46:46 +0200

    Create Views

---
+++ src/views/EventList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Event List</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <BaseIcon />
+  </div>
+</template>
+
+<script>
+import EventCard from "@/components/EventCard.vue";
+import EventService from '@/services/EventService.js';
+
+export default {
+    components: {
+        EventCard
+    },
+    data() {
+        return {
+            events: []
+        }
+    },
+    created() {
+        EventService.getEvents()
+        .then(resp => {
+            this.events = resp.data
+            console.log(resp.data);
+        })
+        .catch(err => {
+            console.log('There was a error' + err.resp);
+        })
+    }
+};
+</script>
